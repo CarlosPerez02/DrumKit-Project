d615fe9e5445997aa450751e20e15c4a1c590b6a
CarlosPerez02 <carlos.perez.moreno.02@gmail.com>
2020-05-03 14:00:53 +0200

    Final Drums

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('keydown', (e) => {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return; 
     audio.currentTime = 0;
     audio.play();
